40ee4dbe1001c59c2e2cf9a62238d583c9086458
Miro Bergam <mbergam2019@pingry.org>
2016-09-25 17:22:55 -0400

    Completed my first version of Staircases.java

--- StairCases.java
+++ StairCases.java
@@ -1,33 +1,58 @@
+import java.util.Scanner; //Import scanner to get user input
+
 /**
- * Write javadoc for your class here (and remove this comment, or course)
- *
+ * This class creates two staircases (ascending and descending) based on user input
+ * @version 0.1
+ * @author Miro Bergam
  */
 
 public class StairCases
-{
+{	
   /**
-   * Main method demonstrates the other wo methods by calling each of them.
+   * Main method demonstrates the other two methods by calling each of them.
    * @param args Standard array of commandline String arguments. Not used in this program.
    */
 
   public static void main(String[] args)
   {
-    //TODO change the next line so it actually gets input from the user
-    int size = 4;
+    Scanner keys = new Scanner(System.in); //establishes scanner
+	System.out.println("What size do you want your staircases to be?");
+	int size = keys.nextInt();  //saves the player's size in the variable "size"
 
-    printAscending(size);
-    printDescending(size);
+    printAscending(size); //calls the ascending staircase method
+    printDescending(size);//calls the descending staircase method
+  }
+  /**
+   * Method prints a descending staircase
+   * @param size The int used to measure the height and width of the descending staircase
+   */
+  public static void printAscending(int size)
+  {
+  	for (int height = 0; height < size; height++) //measures the rows
+  	{
+		for (int length = 0; length < height + 1; length++)//measures the columns 
+		{
+			System.out.print("x ");
+		}
+		System.out.println();
+	}
+  }
+  /**
+   * Method prints a ascending staircase
+   * @param size The int used to measure the height and width of the ascending staircase
+   */
+  public static void printDescending(int size)
+  {
+  	for (int height = size; height > 0; height = height- 1) //measures the rows
+	{
+		for (int length = height; length > 0; length = length - 1) //measures the columns
+		{
+			System.out.print("x "); 
+		}
+		System.out.println();
+	}
   }
 
-  /**
-   * Write your javadoc here
-   */
-  public static void printAscending
-  //TODO Finish this method
 
-  /**
-   * Write your javadoc here
-   */
-  public static void printDescending
-  //TODO Finish this method
+
 }
